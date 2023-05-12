46340cdced10968ee72dbeb563a9b7193efe5efd
Pingvu <nhlpool27@gmail.com>
2023-05-12 14:02:07 +0100

    05-12-2023

--- Projekt/Form1.Designer.cs
+++ Projekt/Form1.Designer.cs
@@ -33,7 +33,6 @@
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -42,6 +41,10 @@
             this.label6 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -53,6 +56,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Starta spelet";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -63,16 +67,18 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Spela igen";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button3.Location = new System.Drawing.Point(247, 293);
+            this.button3.Location = new System.Drawing.Point(214, 71);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Gissa";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
@@ -93,19 +99,10 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Största talet: ";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(36, 196);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(39, 15);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Spelet";
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(36, 249);
+            this.label4.Location = new System.Drawing.Point(6, 28);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(67, 15);
             this.label4.TabIndex = 7;
@@ -114,7 +111,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(36, 292);
+            this.label5.Location = new System.Drawing.Point(0, 43);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(73, 15);
             this.label5.TabIndex = 8;
@@ -122,21 +119,21 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(134, 249);
+            this.textBox2.Location = new System.Drawing.Point(79, 22);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(66, 23);
             this.textBox2.TabIndex = 9;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(134, 293);
+            this.textBox3.Location = new System.Drawing.Point(79, 51);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(66, 23);
             this.textBox3.TabIndex = 10;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(36, 340);
+            this.textBox4.Location = new System.Drawing.Point(6, 77);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(158, 23);
             this.textBox4.TabIndex = 11;
@@ -144,7 +141,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(357, 234);
+            this.label6.Location = new System.Drawing.Point(302, 8);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(76, 15);
             this.label6.TabIndex = 12;
@@ -153,7 +150,7 @@
             // textBox5
             // 
             this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(357, 268);
+            this.textBox5.Location = new System.Drawing.Point(321, 26);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
             this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -163,10 +160,37 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(134, 117);
+            this.comboBox1.Location = new System.Drawing.Point(45, 22);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 23);
             this.comboBox1.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(122, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Speldata";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(36, 200);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(427, 127);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Spelet";
             // 
             // Form1
             // 
@@ -174,22 +198,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(475, 373);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Gissa Tal";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,7 +221,6 @@
         private Button button3;
         private Label label1;
         private Label label2;
-        private Label label3;
         private Label label4;
         private Label label5;
         private TextBox textBox2;
@@ -211,5 +229,7 @@
         private Label label6;
         private TextBox textBox5;
         private ComboBox comboBox1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
     }
 }
--- Projekt/Form1.cs
+++ Projekt/Form1.cs
@@ -2,11 +2,68 @@ namespace Projekt
 {
     public partial class Form1 : Form
     {
+        int datornsTal;
+        int antalGissningar;
+
         public Form1()
         {
             InitializeComponent();
+          
+           label2.Items.Add(10);
+            label2.Items.Add(20);
+           
+           label2.SelectedItem = label2.Items[0];
         }
 
-       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string störstaTal = label2.SelectItem.ToString();
+            int störst = int.Parse(störstaTal);
+            Random r = new Random();
+            datornsTal = r.Next(1, störst + 1);
+
+            
+            button3.Enabled = false;
+            groupBox1.Enabled = false;
+            groupBox2.Enabled = true;
+
+           
+            textBox2.Text = "??";
+          
+            antalGissningar = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            antalGissningar++;
+            string gissa = textBox3.Text;
+            int gissatTal = int.Parse(gissa);
+
+            if (gissatTal == datornsTal)
+            {
+                textBox4.Text = "Korrekt efter" +
+                antalGissningar + " försök.";
+                button2.Enabled = true;
+
+                textBox2.Text = datornsTal.ToString();
+                textBox5.AppendText(antalGissningar + "Försök\n");
+
+            }
+
+            else if (gissatTal < datornsTal)
+            {
+                textBox4.Text = "För lågt. Försök igen.";
+            }
+            else
+            {
+                textBox4.Text = "För högt. Försök igen";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
